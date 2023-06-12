c6eb1e2932ff531d44394056f5c9b3fa9c81840d
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-06-12 14:48:49 +0900

    a-Poeer (その他) の修正

--- 03_その他の問題/a-Power.py
+++ 03_その他の問題/a-Power.py
@@ -1,10 +1,12 @@
 # x の整数乗を効率よく計算
 
-def pow(x: float, n: int):
+def pow(x: float, n: int) -> float:
     if(n == 1): return x
 
     v = pow(x, n//2)
     if(n%2 == 0): return v*v
     else: return v*v*x
 
-print(pow(3, 4))
+x = float(input("x: "))
+n = int(input("n: "))
+print(f"{x} ** {n} = {pow(x, n)}")
